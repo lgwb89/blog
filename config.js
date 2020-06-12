f592d1fc113be51770ef24c9707297eaec18dc07
Louis Barclay <louis.barclay@gmail.com>
2020-06-13 00:41:02 +0100

    tweak photo and update config

--- config.js
+++ config.js
@@ -1,49 +1,49 @@
-'use strict';
+"use strict"
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://louis.work",
+  pathPrefix: "/",
+  title: "louis.work",
+  subtitle: "I write about tech/life balance and other stuff I care about.",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
+      label: "Contact",
+      path: "/pages/contacts",
+    },
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: "louis.work",
+    photo: "/photo.jpg",
+    bio: "Tech addiction, life, etc.",
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
-};
+      email: "",
+      facebook: "",
+      telegram: "",
+      twitter: "",
+      github: "lgwb89",
+      rss: "",
+      vkontakte: "",
+      linkedin: "louis-b-51458a4",
+      instagram: "",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
+}
