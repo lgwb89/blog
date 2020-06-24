47cb1cefbc4ce93e7fb0a808d3e30f06fc608051
Louis Barclay <louis.barclay@gmail.com>
2020-06-24 18:01:05 +0100

    add category

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -1,17 +1,27 @@
 // @flow strict
-import React from 'react';
-import styles from './Content.module.scss';
+import React from "react"
+import { Link } from "gatsby"
+import styles from "./Content.module.scss"
 
 type Props = {
   body: string,
-  title: string
-};
+  title: string,
+  category: string,
+}
 
-const Content = ({ body, title }: Props) => (
-  <div className={styles['content']}>
-    <h1 className={styles['content__title']}>{title}</h1>
-    <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+const Content = ({ body, title, category, categorySlug }: Props) => (
+  <div className={styles["content"]}>
+    <span className={styles["content__category-span"]}>
+      <Link to={categorySlug} className={styles["content__category-link"]}>
+        {category}
+      </Link>
+    </span>
+    <h1 className={styles["content__title"]}>{title}</h1>
+    <div
+      className={styles["content__body"]}
+      dangerouslySetInnerHTML={{ __html: body }}
+    />
   </div>
-);
+)
 
-export default Content;
+export default Content
