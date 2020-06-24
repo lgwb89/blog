fab35c8a1b2cca90df27f8ad4bf6820a8b04bd3c
Louis Barclay <louis.barclay@gmail.com>
2020-06-24 18:11:40 +0100

    add category and categorySlug to query

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,29 +1,38 @@
 // @flow strict
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/Layout';
-import Post from '../components/Post';
-import { useSiteMetadata } from '../hooks';
-import type { MarkdownRemark } from '../types';
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Post from "../components/Post"
+import { useSiteMetadata } from "../hooks"
+import type { MarkdownRemark } from "../types"
 
 type Props = {
   data: {
-    markdownRemark: MarkdownRemark
-  }
-};
+    markdownRemark: MarkdownRemark,
+  },
+}
 
 const PostTemplate = ({ data }: Props) => {
-  const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
-  const { frontmatter } = data.markdownRemark;
-  const { title: postTitle, description: postDescription, socialImage } = frontmatter;
-  const metaDescription = postDescription !== null ? postDescription : siteSubtitle;
+  const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata()
+  const { frontmatter } = data.markdownRemark
+  const {
+    title: postTitle,
+    description: postDescription,
+    socialImage,
+  } = frontmatter
+  const metaDescription =
+    postDescription !== null ? postDescription : siteSubtitle
 
   return (
-    <Layout title={`${postTitle} - ${siteTitle}`} description={metaDescription} socialImage={socialImage} >
+    <Layout
+      title={`${postTitle} - ${siteTitle}`}
+      description={metaDescription}
+      socialImage={socialImage}
+    >
       <Post post={data.markdownRemark} />
     </Layout>
-  );
-};
+  )
+}
 
 export const query = graphql`
   query PostBySlug($slug: String!) {
@@ -33,6 +42,7 @@ export const query = graphql`
       fields {
         slug
         tagSlugs
+        categorySlug
       }
       frontmatter {
         date
@@ -40,9 +50,10 @@ export const query = graphql`
         tags
         title
         socialImage
+        category
       }
     }
   }
-`;
+`
 
-export default PostTemplate;
+export default PostTemplate
