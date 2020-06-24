630e939501d0a04c5c1413654708762bfc057a9f
Louis Barclay <louis.barclay@gmail.com>
2020-06-24 15:51:08 +0100

    add Newsletter

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,29 +1,31 @@
 // @flow strict
-import React from 'react';
-import Author from './Author';
-import Contacts from './Contacts';
-import Copyright from './Copyright';
-import Menu from './Menu';
-import styles from './Sidebar.module.scss';
-import { useSiteMetadata } from '../../hooks';
+import React from "react"
+import Author from "./Author"
+import Contacts from "./Contacts"
+import Copyright from "./Copyright"
+import Menu from "./Menu"
+import Newsletter from "./Newsletter"
+import styles from "./Sidebar.module.scss"
+import { useSiteMetadata } from "../../hooks"
 
 type Props = {
   isIndex?: boolean,
-};
+}
 
 const Sidebar = ({ isIndex }: Props) => {
-  const { author, copyright, menu } = useSiteMetadata();
+  const { author, copyright, menu } = useSiteMetadata()
 
   return (
-    <div className={styles['sidebar']}>
-      <div className={styles['sidebar__inner']}>
+    <div className={styles["sidebar"]}>
+      <div className={styles["sidebar__inner"]}>
         <Author author={author} isIndex={isIndex} />
         <Menu menu={menu} />
+        <Newsletter />
         <Contacts contacts={author.contacts} />
         <Copyright copyright={copyright} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
