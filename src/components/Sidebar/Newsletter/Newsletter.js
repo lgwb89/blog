d984d363565967ae34aeb00e6cf2534822e1a3ca
Louis Barclay <louis.barclay@gmail.com>
2020-06-24 15:51:45 +0100

    create Newsletter component

---
+++ src/components/Sidebar/Newsletter/Newsletter.js
@@ -0,0 +1,58 @@
+// @flow strict
+import React from "react"
+import { Link } from "gatsby"
+import styles from "./Newsletter.module.scss"
+
+type Props = {
+  menu: {
+    label: string,
+    path: string,
+  }[],
+}
+
+class Newsletter extends React.Component {
+  state = {
+    submitted: false,
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      submitted: true,
+    })
+  }
+
+  render() {
+    return (
+      <div className={styles["newsletter"]}>
+        <h4 className={styles["newsletter__title"]}>
+          {!this.state.submitted
+            ? "Get new articles sent to you"
+            : "Thanks for subscribing"}
+        </h4>
+        <form
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          name="newsletter"
+          onSubmit={this.handleSubmit}
+        >
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className={styles["newsletter__form-input"]}
+            placeholder="Your email"
+          />
+          <input type="hidden" name="bot-field" />
+          {/* <input type="hidden" name="form-name" value="contact" /> */}
+          <button type="submit" className={styles["newsletter__form-button"]}>
+            Subscribe
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Newsletter
